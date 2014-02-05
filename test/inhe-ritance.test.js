d168bedbded7dfd42a156c4398595e17763072c3
Thanasis Polychronakis <thanpolas@gmail.com>
2014-02-06 00:56:16 +0200

    enrich tests, add mixins

--- test/inhe-ritance.test.js
+++ test/inhe-ritance.test.js
@@ -93,7 +93,7 @@ suite('2.1 Constructor arguments tests', function() {
     assert.equal(childSingleton.a, 3);
     assert.equal(childSingleton.b, 4);
   });
-  test('2.1.1 Constructor arguments can be stubed by childs', function() {
+  test('2.1.2 Constructor arguments can be stubed by childs', function() {
     var Child = Inhe.extend(function(arg1, arg2) {
       this.a = arg1;
       this.b = arg2;
@@ -113,6 +113,31 @@ suite('2.1 Constructor arguments tests', function() {
     assert.equal(grandChildSingleton.b, 6);
     assert.equal(grandChildSingleton.c, 0);
   });
+
+  test('2.1.3 Constructor stubed arguments do not get confused with fn as args', function() {
+    var Child = Inhe.extend(function(arg1, arg2) {
+      this.a = arg1;
+      this.b = arg2;
+    });
+
+    var GrandChild = Child.extend(function(){this.b++;}, 6, function(arg3) {
+      this.c = arg3;
+    });
+
+    var grandChild = new GrandChild('lol');
+    var grandChildSingleton = GrandChild.getInstance(0);
+
+    assert.isFunction(grandChild.a);
+    grandChild.a();
+    assert.equal(grandChild.b, 7);
+    assert.equal(grandChild.c, 'lol');
+    assert.isFunction(grandChildSingleton.a);
+    grandChildSingleton.a();
+    grandChildSingleton.a();
+    assert.equal(grandChildSingleton.b, 8);
+    assert.equal(grandChildSingleton.c, 0);
+  });
+
 });
 
 suite('2.2 Inheritance tests', function() {
@@ -147,6 +172,137 @@ suite('2.2 Inheritance tests', function() {
 
     var grandChildCopy = GrandChild.getInstance();
     assert.equal(grandChildCopy.a, 2);
+  });
+  test('2.2.4 parent ctor is on "super_"', function() {
+    var Child = Inhe.extend();
+    var GrandChild = Child.extend();
+    var grandChild = GrandChild.getInstance();
+
+    assert.equal(grandChild.super_, Child);
+    assert.equal(Child.getInstance().super_, Inhe);
+  });
+
+});
+
+suite('2.3 Mixins tests', function() {
+  test('2.3.1 Mixin ctor gets invoked', function() {
+    var Child = Inhe.extend(function() {
+      this.a = 1;
+    });
+
+    var ChildToMixin = Inhe.extend(function() {
+      this.b = 2;
+    });
+
+    Child.mixin(ChildToMixin);
+
+    var child = new Child();
+
+    assert.equal(child.a, 1);
+    assert.equal(child.b, 2);
+  });
+  test('2.3.2 Ctor passes the mixin', function() {
+    var Child = Inhe.extend(function() {
+      this.a = 1;
+    });
+
+    var ChildToMixin = Inhe.extend(function() {
+      this.b = 2;
+    });
+
+    Child.mixin(ChildToMixin);
+
+    var GrandChild = Child.extend(function() {
+      this.c = 3;
+    });
+
+    var grandChild = new GrandChild();
+
+    assert.equal(grandChild.a, 1);
+    assert.equal(grandChild.b, 2);
+    assert.equal(grandChild.c, 3);
+  });
+  test('2.3.3 mixin accepts an array of ctors', function() {
+    var Child = Inhe.extend(function() {
+      this.a = 1;
+    });
+
+    var ChildToMixin = Inhe.extend(function() {this.b = 2;});
+    var ChildToMixinTwo = Inhe.extend(function() {this.c = 3;});
+    var ChildToMixinThree = Inhe.extend(function() {this.d = 4;});
+
+    Child.mixin([ChildToMixin, ChildToMixinTwo, ChildToMixinThree]);
+
+    var GrandChild = Child.extend(function() {
+      this.e = 5;
+    });
+
+    var grandChild = new GrandChild();
+
+    assert.equal(grandChild.a, 1);
+    assert.equal(grandChild.b, 2);
+    assert.equal(grandChild.c, 3);
+    assert.equal(grandChild.d, 4);
+    assert.equal(grandChild.e, 5);
+  });
+  test('2.3.4 mixin accepts comma separated ctors', function() {
+    var Child = Inhe.extend(function() {
+      this.a = 1;
+    });
+
+    var ChildToMixin = Inhe.extend(function() {this.b = 2;});
+    var ChildToMixinTwo = Inhe.extend(function() {this.c = 3;});
+    var ChildToMixinThree = Inhe.extend(function() {this.d = 4;});
+
+    Child.mixin(ChildToMixin, ChildToMixinTwo, ChildToMixinThree);
+
+    var GrandChild = Child.extend(function() {
+      this.e = 5;
+    });
+
+    var grandChild = new GrandChild();
+
+    assert.equal(grandChild.a, 1);
+    assert.equal(grandChild.b, 2);
+    assert.equal(grandChild.c, 3);
+    assert.equal(grandChild.d, 4);
+    assert.equal(grandChild.e, 5);
+  });
+  test('2.3.5 Mixin can interact with the this object', function() {
+    var Child = Inhe.extend(function() {
+      this.a = 1;
+    });
+
+    var ChildToMixin = Inhe.extend(function() {
+      this.a += 1;
+    });
+
+    Child.mixin(ChildToMixin);
+    var child = new Child();
+    assert.equal(child.a, 2);
+  });
+
+
+  test('2.3.10 Mixin prototype methods get passed down the chain', function() {
+    var Child = Inhe.extend(function() {
+      this.a = 1;
+    });
+
+    var ChildToMixin = Inhe.extend();
+    ChildToMixin.prototype.add = function(a, b) {
+      return a + b;
+    };
+
+    Child.mixin(ChildToMixin);
+
+    var GrandChild = Child.extend(function() {
+      this.c = 3;
+    });
+
+    var grandChild = new GrandChild();
+
+    assert.isFunction(grandChild.add);
+    assert.equal(grandChild.add(1, 1), 2);
   });
 
 });
