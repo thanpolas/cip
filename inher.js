6ace21bfb75697832930373204d018daed8ce091
Thanasis Polychronakis <thanpolas@gmail.com>
2014-02-07 02:10:26 +0200

    create lib dir and move mixin module there

--- inher.js
+++ inher.js
@@ -108,34 +108,6 @@ function generateRandomString(optLength) {
 }
 
 /**
- * Mixin method.
- *
- * @param {Inhe} Ctor ctor to mixin onto.
- * @param {...Function|...Inhe||Array.<Function>} args any combination of
- *   constructors to mixin, check documentation.
- * @return {void}
- */
-Inhe.mixin = function(Ctor) {
-  var args = Array.prototype.slice.call(arguments, 1);
-  var mixinCtors = [];
-
-  args.forEach(function(arg) {
-    if (typeof(arg) === 'function') {
-      mixinCtors.push(arg);
-    } else if (Array.isArray(arg)) {
-      mixinCtors = mixinCtors.concat(arg);
-    } else {
-      throw new TypeError('Mixin arguments not of right type');
-    }
-  });
-
-  mixinCtors.forEach(function(MixinCtor) {
-    assign(Ctor.prototype, MixinCtor.prototype);
-    Ctor._inhe.mixins.push(MixinCtor);
-  });
-};
-
-/**
  * Returns a singleton instance of the provided Ctor.
  *
  * @param {Inhe} ParentCtor Inhe inherited Ctor.
