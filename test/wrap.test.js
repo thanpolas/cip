2e3a6cacb89cb4e9008f2ad3c736c7a2b6b42089
Thanasis Polychronakis <thanpolas@gmail.com>
2014-02-16 22:22:32 +0200

    change library symbols and fix typos by replace

--- test/wrap.test.js
+++ test/wrap.test.js
@@ -27,7 +27,7 @@ suite('Wrap tests', function() {
 
   test('Wrap adds all Cip props and methods', function() {
     var CipTypicalCtor = Cip.wrap(TypicalCtor);
-    assert.ok(Cip.isCip(CipTypicalCtor));
+    assert.ok(Cip.is(CipTypicalCtor));
     assert.isFunction(CipTypicalCtor.extend, 'CipTypicalCtor should have an "extend" static method');
     assert.isFunction(CipTypicalCtor.getInstance, 'CipTypicalCtor should have a "getInstance" static method');
     assert.notProperty(CipTypicalCtor, 'wrap', 'CipTypicalCtor should *not* have a "wrap" static method');
@@ -44,6 +44,6 @@ suite('Wrap tests', function() {
     var Child = CipTypicalCtor.extend();
 
     assert.notProperty(TypicalCtor, 'extend');
-    assert.notOk(Cip.isCip(TypicalCtor));
+    assert.notOk(Cip.is(TypicalCtor));
   });
 });
